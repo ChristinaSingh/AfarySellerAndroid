a2989839892c30113b925ce95d63edb6edef9dcb
keshav <keshav.technorizen@gmail.com>
2025-06-18 19:31:18 +0530

    update code  18-06-2025 by Keshav Savita

--- app/src/main/java/com/afaryseller/ui/wallet/WalletFragment.java
+++ app/src/main/java/com/afaryseller/ui/wallet/WalletFragment.java
@@ -189,7 +189,6 @@ public class WalletFragment extends Fragment implements AskListener {
         });
     }
 
-
     private void setAdapter() {
 
         mAdapter = new WalletAdapter(getActivity(), get_result);
